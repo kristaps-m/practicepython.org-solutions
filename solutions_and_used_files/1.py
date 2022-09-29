45e60e305e7aa32ebb1664c78a20d4c6f2dec236
Kristaps Mitins <61430801+kristaps-m@users.noreply.github.com>
2022-09-29 13:31:02 +0300

    Update 1.py

--- solutions_and_used_files/1.py
+++ solutions_and_used_files/1.py
@@ -1,1 +1,7 @@
 
+n = input("Whats ur name? :...");
+a = int(input("How old are u ? ... : "));
+t = int(input("How many time print? ..."));
+
+for i in range(t):
+    print("Hi, {} you will be 100 in year {} !".format(n,2020-a+100));
